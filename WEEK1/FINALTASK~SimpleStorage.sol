ad33dccbb87ea09cc29347dc863d5188c8d6cae5
Yash_10 <yashyt01@gmail.com>
2026-01-04 16:31:34 +0530

    deployment details of SimpleStorage contract
    Added deployment information for the SimpleStorage contract.

--- WEEK1/FINALTASK~SimpleStorage.sol
+++ WEEK1/FINALTASK~SimpleStorage.sol
@@ -19,3 +19,7 @@ contract SimpleStorage{
      }
     
 }
+// DEPLOYED LOCALLY IN ANVIL BLOCKCHAIN...
+//Deployer: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+//Deployed to: 0x610178dA211FEF7D417bC0e6FeD39F05609AD788
+//Transaction hash: 0x29783b9cb2cfa5cb19904a31fa4895c7e3f3ee91ef23ea95320a70b58062d0be
