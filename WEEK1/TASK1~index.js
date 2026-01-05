1061f6a8339c5a3f3818b121d1ad40a87054ab99
Yash_10 <yashyt01@gmail.com>
2026-01-05 22:56:18 +0530

    Add Ethereum contract interaction functions

---
+++ WEEK1/TASK1~index.js
@@ -0,0 +1,57 @@
+import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@6.8.1/dist/ethers.min.js";
+//OR
+//  import {ethers} from "ethers"
+const abi=[
+        {
+            "type": "function",
+            "name": "get",
+            "inputs": [],
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "stateMutability": "view"
+        },
+        {
+            "type": "function",
+            "name": "set",
+            "inputs": [
+                {
+                    "name": "_number",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "outputs": [],
+            "stateMutability": "nonpayable"
+        }
+    ]
+  const address="0xB3030DcCCcB270ae1422EcaA9edd57CD5911D665";   
+    async function connect(){
+       await window.ethereum.request({ method: "eth_requestAccounts"  }); 
+    }
+    async function set(){
+      
+       const provider=new ethers.BrowserProvider(window.ethereum);
+       const signer= await provider.getSigner();
+       const Contract= new ethers.Contract(address,abi,signer);
+       const no=Number(document.getElementById("inputurFavNumber").value);
+       const transactioninfo = await Contract.set(no);  
+       console.log("Transaction Hash:", transactioninfo.hash);
+      document.getElementById("txHash").innerText = transactioninfo.hash;
+
+    }
+    async function get(){
+       const provider=new ethers.BrowserProvider(window.ethereum);
+       const Contract= new ethers.Contract(address,abi,provider)
+       const favoritenumber=await Contract.get();
+       document.getElementById("output").innerText=favoritenumber.toString();
+
+    }
+
+window.connect = connect;
+window.set = set;
+window.get = get;
