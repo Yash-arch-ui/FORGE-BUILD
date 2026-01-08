de01d5277c025cc2207df582fa5ca95ae3af54e1
Yash_10 <yashyt01@gmail.com>
2026-01-09 00:47:10 +0530

    Refactor deposit function and add address tracking

--- WEEK2 /Ledger.sol
+++ WEEK2 /Ledger.sol
@@ -1,13 +1,18 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 contract Ledger{
-    mapping(address => uint256) private  s_deposits;
+    mapping(address => uint256) private  s_addressToAmountDeposited;
+    address[] public s_deposits;
     error Ledger__NotEnoughEthSent();
     event Deposit(address  indexed USER,uint256 amount);
     function deposit() public payable{
-           if( (msg.value)<= 0)
+           if( (msg.value) == 0)
         revert Ledger__NotEnoughEthSent();
-        s_deposits[msg.sender]=s_deposits[msg.sender]+ msg.value;
+        s_addressToAmountDeposited[msg.sender]=s_addressToAmountDeposited[msg.sender]+ msg.value;
+        s_deposits.push(msg.sender);
         emit Deposit(msg.sender,msg.value);
+    }
+    function getAddresstoAmountDeposited(address depositingaddress) external view returns (uint256){
+   return s_addressToAmountDeposited[depositingaddress];
     }
 }
